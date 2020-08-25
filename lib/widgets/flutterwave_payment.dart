0dc95da5ccf89922e8c42430c30b04e14be6af57
freddominant <fredadewole@gmail.com>
2020-08-25 13:52:12 +0100

    add payment screen

--- lib/widgets/flutterwave_payment.dart
+++ lib/widgets/flutterwave_payment.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutterwave/core/flutterwave_payment_manager.dart';
 
+import 'flutterwave_payment_option.dart';
+
 class FlutterwaveUI extends StatefulWidget {
   FlutterwaveUI(FlutterwavePaymentManager paymentManager);
 
@@ -15,39 +17,93 @@ class _FlutterwaveUIState extends State<FlutterwaveUI> {
       home: Scaffold(
         body: Container(
           width: double.infinity,
-          child: Column(
+          child: ListView(
             children: [
-              Row(
-                children: [
-                  Icon(
-                    Icons.lock,
-                    size: 5.0,
-                    color: Colors.black,
-                  ),
-                  Text(
-                    "SECURED BY FLUTTERWAVE",
-                    style: TextStyle(
-                        color: Colors.black,
-                        fontSize: 10.0,
-                        height: 10.0,
-                        letterSpacing: 3.0),
-                  )
-                ],
+              Container(
+                margin: EdgeInsets.fromLTRB(8.0, 8.0, 0, 8.0),
+                child: Column(
+                  children: [
+                    Row(
+                      mainAxisSize: MainAxisSize.max,
+                      children: [
+                        Icon(
+                          Icons.lock,
+                          size: 10.0,
+                          color: Colors.black,
+                        ),
+                        SizedBox(
+                          width: 5.0,
+                        ),
+                        Text(
+                          "SECURED BY FLUTTERWAVE",
+                          style: TextStyle(
+                              color: Colors.black,
+                              fontSize: 10.0,
+                              letterSpacing: 1.0),
+                        )
+                      ],
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 150.0,
+                    ),
+                    Container(
+                      width: double.infinity,
+                      margin: EdgeInsets.zero,
+                      child: Text(
+                        "How would you \nlike to pay?",
+                        textAlign: TextAlign.left,
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          color: Colors.black,
+                          fontSize: 30.0,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
               ),
               SizedBox(
                 width: double.infinity,
-                height: 50.0,
+                height: 150.0,
               ),
               Container(
-                width: MediaQuery.of(context).size.width / 3,
-                child: Text(
-                  "How would you like to pay",
-                  style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      color: Colors.black,
-                      fontSize: 30.0,
-                      height: 10.0,
-                      letterSpacing: 3.0),
+                color: Colors.white38,
+                width: double.infinity,
+                child: Column(
+                  children: [
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: FlutterwavePaymentOption(
+                          handleClick: () => {}, buttonText: "Account"),
+                    ),
+                    SizedBox(
+                      height: 0.5,
+                    ),
+                    SizedBox(
+                      width: double.infinity,
+                      height: 50.0,
+                      child: FlutterwavePaymentOption(
+                          handleClick: () => {}, buttonText: "card"),
+                    ),
+                    SizedBox(
+                      height: 0.5,
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                      child: FlutterwavePaymentOption(
+                          handleClick: () => {}, buttonText: "bank transfer"),
+                    ),
+                    SizedBox(
+                      height: 0.5,
+                    ),
+                    SizedBox(
+                      height: 50.0,
+                      child: FlutterwavePaymentOption(
+                          handleClick: () => {}, buttonText: "ussd"),
+                    ),
+                  ],
                 ),
               ),
             ],
